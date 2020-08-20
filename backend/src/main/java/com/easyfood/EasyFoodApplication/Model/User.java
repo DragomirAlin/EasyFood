941cc090b2017dfa98c4f780df46fadd672f1125
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-08-20 13:19:21 +0300

    created the necessary classes for the logical user

---
+++ backend/src/main/java/com/easyfood/EasyFoodApplication/Model/User.java
@@ -0,0 +1,9 @@
+package com.easyfood.EasyFoodApplication.Model;
+
+
+public class User {
+
+    private String username;
+    private String password;
+
+}
---
+++ backend/src/main/java/com/easyfood/EasyFoodApplication/Repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.easyfood.EasyFoodApplication.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
