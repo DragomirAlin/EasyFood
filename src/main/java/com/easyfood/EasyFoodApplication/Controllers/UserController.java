ef043b2b8e88591a8d7cf86fa641ad4fd5851e2d
DragomirAlin <dragomirdanielalin@Yahoo.com>
2020-09-25 15:39:29 +0300

    added user service and role service

--- src/main/java/com/easyfood/EasyFoodApplication/Controllers/UserController.java
+++ src/main/java/com/easyfood/EasyFoodApplication/Controllers/UserController.java
@@ -2,18 +2,32 @@ package com.easyfood.EasyFoodApplication.Controllers;
 
 import com.easyfood.EasyFoodApplication.Models.User;
 import com.easyfood.EasyFoodApplication.Security.service.UserDetailsServiceImpl;
+import com.easyfood.EasyFoodApplication.Service.MenuService.ProductService;
+import com.easyfood.EasyFoodApplication.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test/user")
+@RequestMapping("/api/test/user/")
 public class UserController {
 
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/viewUser/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_MODERATOR') or hasRole('ROLE_ADMIN')")
+    public Optional<User> getUser(@PathVariable String email){
+        return userService.loadUserFromEmail(email);
     }
+
+}
+
 
---
+++ src/main/java/com/easyfood/EasyFoodApplication/Service/RoleService.java
@@ -0,0 +1,4 @@
+package com.easyfood.EasyFoodApplication.Service;
+
+public class RoleService {
+}
